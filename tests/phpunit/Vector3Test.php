34af71a932c095d976d25407016977f1759c057b
CortexPE <mcpe4life62@gmail.com>
2019-04-16 04:48:31 -0700

    Remove random number tests & test decimals

--- tests/phpunit/Vector3Test.php
+++ tests/phpunit/Vector3Test.php
@@ -23,46 +23,32 @@ namespace pocketmine\math;
 
 use PHPUnit\Framework\TestCase;
 
-class Vector3Test extends TestCase{
-	
-	protected function generateRandIntArray(int $count = 3, int $min = -100, $max = 100) : array {
-		$arr = [];
-		for($i = 0; $i < $count; $i++){
-			$arr[] = mt_rand($min, $max);
-		}
-		return $arr;
-	}
+class Vector3Test extends TestCase {
 
-	public function minProvider() : \Generator{
+	public function minProvider(): \Generator{
 		yield [
-			new Vector3(0, mt_rand(0, 100), mt_rand(0, 100)),
-			new Vector3(mt_rand(0, 100), 0, mt_rand(0, 100)),
-			new Vector3(mt_rand(0, 100), mt_rand(0, 100), 0),
-			new Vector3(0, 0, 0)
+			new Vector3(1, 2, 3),
+			new Vector3(9, 8, 7),
+			new Vector3(3, 2, 1),
+			new Vector3(1, 2, 1)
 		];
 		yield [
-			new Vector3(mt_rand(0, 100), mt_rand(0, 100), 0),
-			new Vector3(4, mt_rand(0, 100), mt_rand(0, 100)),
-			new Vector3(mt_rand(0, 100), 2, mt_rand(0, 100)),
-			new Vector3(4, 2, 0)
+			new Vector3(1, 2, -3),
+			new Vector3(9, -8, 7),
+			new Vector3(-9, 2, 1),
+			new Vector3(-9, -8, -3)
 		];
 		yield [
-			new Vector3(1337, 256, 512),
-			new Vector3(9, 9, 9),
-			new Vector3(128, 256, 512),
-			new Vector3(9, 9, 9)
+			new Vector3(1.23, 6.28, 18.84),
+			new Vector3(3.14, 2.34, 9.42),
+			new Vector3(6.28, 12.56, 3.45),
+			new Vector3(1.23, 2.34, 3.45)
 		];
-		$arrRandX = $this->generateRandIntArray();
-		$minX = min($arrRandX);
-		$arrRandY = $this->generateRandIntArray();
-		$minY = min($arrRandY);
-		$arrRandZ = $this->generateRandIntArray();
-		$minZ = min($arrRandZ);
 		yield [
-			new Vector3($arrRandX[0], $arrRandY[0], $arrRandZ[0]),
-			new Vector3($arrRandX[1], $arrRandY[1], $arrRandZ[1]),
-			new Vector3($arrRandX[2], $arrRandY[2], $arrRandZ[2]),
-			new Vector3($minX, $minY, $minZ)
+			new Vector3(-M_PI, M_PI ** 2, M_PI ** 3),
+			new Vector3(M_PI, -(M_PI ** 2), 0),
+			new Vector3(0, 0, -(M_PI ** 3)),
+			new Vector3(-M_PI, -(M_PI ** 2), -(M_PI ** 3))
 		];
 	}
 
@@ -74,40 +60,34 @@ class Vector3Test extends TestCase{
 	 * @param Vector3 $vec3
 	 * @param Vector3 $expected
 	 */
-	public function testMin(Vector3 $vec1, Vector3 $vec2, Vector3 $vec3, Vector3 $expected) : void{
+	public function testMin(Vector3 $vec1, Vector3 $vec2, Vector3 $vec3, Vector3 $expected): void {
 		self::assertEquals(Vector3::min($vec1, $vec2, $vec3), $expected);
 	}
 
 	public function maxProvider() : \Generator{
 		yield [
-			new Vector3(1000, mt_rand(0, 100), mt_rand(0, 100)),
-			new Vector3(mt_rand(0, 100), 1000, mt_rand(0, 100)),
-			new Vector3(mt_rand(0, 100), mt_rand(0, 100), 1000),
-			new Vector3(1000, 1000, 1000)
+			new Vector3(1, 2, 3),
+			new Vector3(9, 8, 7),
+			new Vector3(3, 2, 1),
+			new Vector3(9, 8, 7)
 		];
 		yield [
-			new Vector3(mt_rand(-100, 0), mt_rand(-100, 0), 0),
-			new Vector3(4, mt_rand(-100, 0), mt_rand(-100, 0)),
-			new Vector3(mt_rand(-100, 0), 2, mt_rand(-100, 0)),
-			new Vector3(4, 2, 0)
+			new Vector3(1, 2, -3),
+			new Vector3(9, -8, 7),
+			new Vector3(-9, 2, 1),
+			new Vector3(9, 2, 7)
 		];
 		yield [
-			new Vector3(1337, 256, 512),
-			new Vector3(9, 9, 9),
-			new Vector3(128, 256, 512),
-			new Vector3(1337, 256, 512)
+			new Vector3(1.23, 6.28, 18.84),
+			new Vector3(3.14, 2.34, 9.42),
+			new Vector3(6.28, 12.56, 3.45),
+			new Vector3(6.28, 12.56, 18.84)
 		];
-		$arrRandX = $this->generateRandIntArray();
-		$maxX = max($arrRandX);
-		$arrRandY = $this->generateRandIntArray();
-		$maxY = max($arrRandY);
-		$arrRandZ = $this->generateRandIntArray();
-		$maxZ = max($arrRandZ);
 		yield [
-			new Vector3($arrRandX[0], $arrRandY[0], $arrRandZ[0]),
-			new Vector3($arrRandX[1], $arrRandY[1], $arrRandZ[1]),
-			new Vector3($arrRandX[2], $arrRandY[2], $arrRandZ[2]),
-			new Vector3($maxX, $maxY, $maxZ)
+			new Vector3(-M_PI, M_PI ** 2, M_PI ** 3),
+			new Vector3(M_PI, -(M_PI ** 2), 0),
+			new Vector3(0, 0, -(M_PI ** 3)),
+			new Vector3(M_PI, M_PI ** 2, M_PI ** 3)
 		];
 	}
 
@@ -119,7 +99,7 @@ class Vector3Test extends TestCase{
 	 * @param Vector3 $vec3
 	 * @param Vector3 $expected
 	 */
-	public function testMax(Vector3 $vec1, Vector3 $vec2, Vector3 $vec3, Vector3 $expected) : void{
+	public function testMax(Vector3 $vec1, Vector3 $vec2, Vector3 $vec3, Vector3 $expected): void {
 		self::assertEquals(Vector3::max($vec1, $vec2, $vec3), $expected);
 	}
 }
