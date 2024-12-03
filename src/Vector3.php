f25c60812376f6f3eaab42314899d5b55166d89f
Dylan T. <dktapps@pmmp.io>
2024-12-03 12:35:09 +0000

    github web editor try not to suck challenge IMPOSSIBLE

--- src/Vector3.php
+++ src/Vector3.php
@@ -190,7 +190,7 @@ class Vector3{
 
 	/**
 	 * Yields vectors stepped out from this one in directions except those on the given axis.
-  	 *
+	 *
 	 * @param Axis $axis Facing directions on this axis will be excluded
 	 *
 	 * @return \Generator|Vector3[]
